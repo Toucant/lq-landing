6ddd45424eab16958a9efdc696e30d671d5f5a94
Toucant <cnea1997@gmail.com>
2021-05-20 16:09:59 -0500

    Add build

---
+++ build/precache-manifest.ad7d198c3493c108a5fbb3b92ffe1762.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3489280c225e1d942ea3af5ee49fa528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be7a9c5cbe918afd6bc2",
+    "url": "/static/css/main.18cecdeb.chunk.css"
+  },
+  {
+    "revision": "e8f560d32574818c8c1d",
+    "url": "/static/js/2.4662d0e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4662d0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7a9c5cbe918afd6bc2",
+    "url": "/static/js/main.9ab10ce0.chunk.js"
+  },
+  {
+    "revision": "08c78942dae95c4c4d5f",
+    "url": "/static/js/runtime-main.f68614c4.js"
+  },
+  {
+    "revision": "cfe0b20cdc30a7ea827d6977c6f2dac4",
+    "url": "/static/media/Liqwid-render-green-purplev2@2x.cfe0b20c.png"
+  },
+  {
+    "revision": "dd3bb2d4daf3bc20583955e04721efea",
+    "url": "/static/media/advisor-ac.dd3bb2d4.png"
+  },
+  {
+    "revision": "5e597987c890f8a099be99eb5d767f65",
+    "url": "/static/media/advisor-hp.5e597987.png"
+  },
+  {
+    "revision": "c9231dba9b7030c679a76377800ce57e",
+    "url": "/static/media/advisor-ld.c9231dba.png"
+  },
+  {
+    "revision": "9bb6fbdd38748e71e031e6b1d990bc0b",
+    "url": "/static/media/dc.9bb6fbdd.png"
+  },
+  {
+    "revision": "9c123b96bedc8fb9b8621f072e137350",
+    "url": "/static/media/fv.9c123b96.png"
+  },
+  {
+    "revision": "2ca1a3b653f11e3f26fedac35b4ccabf",
+    "url": "/static/media/hh.2ca1a3b6.png"
+  },
+  {
+    "revision": "acd3157b03cf02dc8ee89137305f65e3",
+    "url": "/static/media/ja.acd3157b.png"
+  },
+  {
+    "revision": "05a27c96ad9129b96a71c82135419a50",
+    "url": "/static/media/liqwid_FINAL_v2-01.05a27c96.png"
+  },
+  {
+    "revision": "fc4c741123b9ce1e77c17e755350d41e",
+    "url": "/static/media/tv.fc4c7411.png"
+  }
+]);
